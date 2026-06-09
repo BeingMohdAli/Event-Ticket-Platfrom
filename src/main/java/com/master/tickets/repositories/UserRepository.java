4491eacecb13473e94fb8a6cb5645ea72f856343
BeingMohdAli <mohdalisaad868@gmail.com>
2026-06-09 13:17:51 +0530

    working on authenticating user and saviing to db

---
+++ src/main/java/com/master/tickets/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.master.tickets.repositories;
+
+import com.master.tickets.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+
+
+
+}
