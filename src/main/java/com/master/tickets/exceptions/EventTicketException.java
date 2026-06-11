1c90e190da5448590d43ca4f5e499dd7bd79002f
BeingMohdAli <mohdalisaad868@gmail.com>
2026-06-11 13:38:29 +0530

    working on Exceptions

---
+++ src/main/java/com/master/tickets/exceptions/EventTicketException.java
@@ -0,0 +1,23 @@
+package com.master.tickets.exceptions;
+
+public class EventTicketException extends Exception{
+
+    public EventTicketException() {
+    }
+
+    public EventTicketException(String message) {
+        super(message);
+    }
+
+    public EventTicketException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EventTicketException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public EventTicketException(Throwable cause) {
+        super(cause);
+    }
+}
