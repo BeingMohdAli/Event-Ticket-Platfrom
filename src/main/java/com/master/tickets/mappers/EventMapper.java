693e9dd1e83f74e8d1e6906564c61612ce476817
BeingMohdAli <mohdalisaad868@gmail.com>
2026-06-15 12:53:25 +0530

    working on Mappers

---
+++ src/main/java/com/master/tickets/mappers/EventMapper.java
@@ -0,0 +1,21 @@
+package com.master.tickets.mappers;
+
+import com.master.tickets.domain.CreateEventRequesting;
+import com.master.tickets.domain.CreateTicketTypeRequest;
+import com.master.tickets.domain.dtos.CreateEventRequestDto;
+import com.master.tickets.domain.dtos.CreateEventResponseDto;
+import com.master.tickets.domain.dtos.CreateTicketTypeRequestTypeDto;
+import com.master.tickets.domain.entities.Event;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface EventMapper {
+
+    CreateTicketTypeRequest fromDto(CreateTicketTypeRequestTypeDto dto);
+
+    CreateEventRequesting fromDto(CreateEventRequestDto dto);
+
+    CreateEventResponseDto toDto(Event event);
+
+}
