1590cdea5e228ee0dbc3837a8abb2e4997962b44
BeingMohdAli <mohdalisaad868@gmail.com>
2026-06-16 20:30:32 +0530

    working on Errors

--- src/main/java/com/master/tickets/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/master/tickets/controllers/GlobalExceptionHandler.java
@@ -5,21 +5,51 @@ import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDto> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException ex) {
+
+        log.error("Caught MethodArgumentNotValidException", ex);
+        ErrorDto errorDto = new ErrorDto();
+
+        BindingResult bindingResult = ex.getBindingResult();
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        String errorMessage = fieldErrors.stream()
+                .findFirst()
+                .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+                .orElse("Validation error occurred");
+
+        errorDto.setError(errorMessage);
+        return new ResponseEntity<>(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ErrorDto> handleConstraintViolation(
             ConstraintViolationException ex) {
-
         log.error("caught ConstraintViolationException", ex);
         ErrorDto errorDto = new ErrorDto();
+
+        ex.getConstraintViolations().stream()
+                .findFirst().map(violation->violation.
+                        getPropertyPath()+ " : " + violation.getMessage()
+                ).orElse("Constraint violation occurred");
+
         errorDto.setError("An unknown error occurred");
-        return new ResponseEntity<>(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
+
+        return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
