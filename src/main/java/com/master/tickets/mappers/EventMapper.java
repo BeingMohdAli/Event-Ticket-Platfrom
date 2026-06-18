983c69c2dbecbce6cb329e16419515a5a67f1266
BeingMohdAli <mohdalisaad868@gmail.com>
2026-06-18 18:25:48 +0530

    working on dtos

--- src/main/java/com/master/tickets/mappers/EventMapper.java
+++ src/main/java/com/master/tickets/mappers/EventMapper.java
@@ -2,10 +2,7 @@ package com.master.tickets.mappers;
 
 import com.master.tickets.domain.CreateEventRequesting;
 import com.master.tickets.domain.CreateTicketTypeRequest;
-import com.master.tickets.domain.dtos.CreateEventRequestDto;
-import com.master.tickets.domain.dtos.CreateEventResponseDto;
-import com.master.tickets.domain.dtos.CreateTicketTypeRequestTypeDto;
-import com.master.tickets.domain.dtos.ListEventTicketTypeResponseDto;
+import com.master.tickets.domain.dtos.*;
 import com.master.tickets.domain.entities.Event;
 import com.master.tickets.domain.entities.TicketType;
 import org.mapstruct.Mapper;
@@ -22,6 +19,8 @@ public interface EventMapper {
     CreateEventResponseDto toDto(Event event);
 
     ListEventTicketTypeResponseDto toDto(TicketType ticketType);
+
+    ListEventResponseDto toListEventResponseDto(Event event);
 
 
 }
